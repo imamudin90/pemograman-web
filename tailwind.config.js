53a0cd8c7dd0114e15fcff19ed699683975b6cb8
imamudin90 <115957425+imamudin90@users.noreply.github.com>
2022-11-20 05:24:53 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0EC5D7',
+                secondary: '#ff3b07',
+                nav: '#969696',
+
+            },
+            
+            fontFamily: {
+                poppins: ['poppins', 'sans-serif'],
+                inter :['Inter', 'sans-serif'],
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
+            }
+        },
+    },
+}
